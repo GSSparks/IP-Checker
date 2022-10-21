0ebd2304fde7e711cc63d17c702da4f721035fe2
Gary Sparks <112287133+GSSparks@users.noreply.github.com>
2022-10-20 21:22:27 -0400

    Update ipcheck.sh

--- ipcheck.sh
+++ ipcheck.sh
@@ -9,6 +9,6 @@ if [ "${ip_add}" == "${ip_add_challenge}" ]; then
     exit
 else
     echo 'IP change is detected'
-    echo "IP change is detected. Old IP was $ip_add. New IP is $ip_add_challenge." | mail -s 'IP Change Detected' -r 'information@garysparks.info' garypriscillasparks@gmail.com
+    echo "IP change is detected. Old IP was $ip_add. New IP is $ip_add_challenge." | mail -s 'IP Change Detected' -r 'information@mail.com' emailaddress@mail.com
     echo $ip_add_challenge > .public-ip
 fi
